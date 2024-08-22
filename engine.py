301377bc255e6e6debfbbb868f33cde3061eaa25
lorenz369 <lorenz369@outlook.de>
2024-08-22 11:02:46 -0700

    Debug AMP

--- engine.py
+++ engine.py
@@ -76,8 +76,8 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
             if profiling_section == 'forward' or profiling_section == 'all': # Added by Marco Lorenz on April 2nd, 2024
                 cupy.cuda.runtime.profilerStart()
             outputs = model(samples)
-            print(f"Logits type: {output["pred_logits"].dtype}")
-            print(f"Boxes type: {output["pred_boxes"].dtype}")
+            print(f"Logits type: {outputs["pred_logits"].dtype}")
+            print(f"Boxes type: {outputs["pred_boxes"].dtype}")
             if profiling_section == 'forward': # Added by Marco Lorenz on April 2nd, 2024
                 cupy.cuda.runtime.profilerStop()
 
