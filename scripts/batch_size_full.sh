6b68e70669b27377b326cfff131c9742c0affcfe
lorenz369 <lorenz369@outlook.de>
2024-08-23 14:53:27 -0700

    Add batch_size scripts

--- scripts/batch_size_full.sh
+++ scripts/batch_size_full.sh
@@ -11,6 +11,6 @@ python="python /global/homes/m/marcolz/DETR/hgp_detr/main.py"
 
 for batch_size in 2 4 8 16 32 64 128 256
 do
-    output="/global/homes/m/marcolz/DETR/batch_size_$batch_size.out"
+    output="/global/homes/m/marcolz/DETR/batch_size_{$batch_size}_full.out"
     $python --epochs 3 --batch_size $batch_size --dataset_file hgp --use_amp | tee -a $output
 done
