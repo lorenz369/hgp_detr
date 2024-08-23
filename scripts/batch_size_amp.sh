336248892dd65c3a50f5ab37394d087c8d70d435
lorenz369 <lorenz369@outlook.de>
2024-08-23 15:10:35 -0700

    Add environment cmds

--- scripts/batch_size_amp.sh
+++ scripts/batch_size_amp.sh
@@ -7,10 +7,15 @@
 #SBATCH --account=m2845
 #SBATCH --output=/global/homes/m/marcolz/DETR/batch_size_%j.out
 
+module load conda
+conda activate detr_12.2
+export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
+export MASTER_PORT=12345
+
 python="python /global/homes/m/marcolz/DETR/hgp_detr/main.py"
 
 for batch_size in 2 4 8 16 32 64 128 256
 do
-    output="/global/homes/m/marcolz/DETR/batch_size_{$batch_size}_amp.out"
+    output="/global/homes/m/marcolz/DETR/batch_size_amp_$batch_size.out"
     $python --epochs 3 --batch_size $batch_size --dataset_file hgp | tee -a $output
 done
