017073257028dc0f557aa64b9ce7145045932825
lorenz369 <lorenz369@outlook.de>
2024-08-23 15:53:49 -0700

    Update batch-size scripts

--- scripts/batch_size_amp.sh
+++ scripts/batch_size_amp.sh
@@ -14,7 +14,7 @@ export MASTER_PORT=12345
 
 python="python /global/homes/m/marcolz/DETR/hgp_detr/main.py"
 
-for batch_size in 2 4 8 16 32 64 128 256
+for batch_size in 16 32 64 128 256
 do
     output="/global/homes/m/marcolz/DETR/batch_size_amp_$batch_size.out"
     $python --epochs 3 --batch_size $batch_size --dataset_file hgp | tee -a $output
